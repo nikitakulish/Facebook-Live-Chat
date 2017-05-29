9598430ab120c4ffdf096b281948e244d367bcbe
Nikita Kulish <nikitakulish@gmail.com>
2017-05-29 20:04:29 +0200

    First upload

---
+++ fb-chat/js/fb-live-chat-init.js
@@ -0,0 +1,10 @@
+fbLiveChat.init({
+    sdk_locale: 'en_EN',
+    facebook_page: 'Nikita-Kulish-1949081661986810', // required
+    position: 'right',
+    header_text: 'Fast contact',
+    header_background_color: '#4267b2',
+    show_close_btn: false,
+    animation_speed: 400,
+    auto_show_delay: 0
+});
